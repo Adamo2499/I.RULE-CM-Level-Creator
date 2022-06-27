45964f2ac66c133a982657032e6bb2cbdbee8d49
Adamo2499 <30226946+Adamo2499@users.noreply.github.com>
2022-06-27 18:25:19 +0200

    Added more variables to customize

--- I.RULE CM Level Creator/Form1.Designer.cs
+++ I.RULE CM Level Creator/Form1.Designer.cs
@@ -31,13 +31,26 @@
             this.label1 = new System.Windows.Forms.Label();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.colorDialog2 = new System.Windows.Forms.ColorDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.levelNameTextBox = new System.Windows.Forms.TextBox();
+            this.levelDescriptionTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,19 +63,19 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Level\'s name:";
             // 
-            // textBox1
+            // levelNameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(129, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 0;
+            this.levelNameTextBox.Location = new System.Drawing.Point(170, 28);
+            this.levelNameTextBox.Name = "levelNameTextBox";
+            this.levelNameTextBox.Size = new System.Drawing.Size(125, 27);
+            this.levelNameTextBox.TabIndex = 0;
             // 
-            // textBox2
+            // levelDescriptionTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(170, 62);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(220, 27);
-            this.textBox2.TabIndex = 2;
+            this.levelDescriptionTextBox.Location = new System.Drawing.Point(170, 62);
+            this.levelDescriptionTextBox.Name = "levelDescriptionTextBox";
+            this.levelDescriptionTextBox.Size = new System.Drawing.Size(220, 27);
+            this.levelDescriptionTextBox.TabIndex = 2;
             // 
             // label2
             // 
@@ -75,7 +88,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(260, 31);
+            this.button1.Location = new System.Drawing.Point(301, 29);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(206, 29);
             this.button1.TabIndex = 3;
@@ -93,24 +106,9 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(7, 10);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(781, 428);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Create new level";
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(27, 380);
+            this.button3.Location = new System.Drawing.Point(27, 722);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 29);
             this.button3.TabIndex = 5;
@@ -118,11 +116,134 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Hearts falling speed: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hearts value: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(171, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Starting hearts:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(244, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Hearts\' disappering timer starts on: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Hearts limit:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(170, 137);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 300);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Max numer of vawes: ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(185, 223);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 27);
+            this.textBox6.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 297);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(277, 177);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.levelDescriptionTextBox);
+            this.groupBox1.Controls.Add(this.levelNameTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(7, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(781, 770);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create new level";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 792);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -139,12 +260,25 @@
         private Label label1;
         private ColorDialog colorDialog1;
         private ColorDialog colorDialog2;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private SaveFileDialog saveFileDialog1;
+        private TextBox levelNameTextBox;
+        private TextBox levelDescriptionTextBox;
         private Label label2;
         private Button button1;
         private Button button2;
-        private GroupBox groupBox1;
         private Button button3;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label6;
+        private Label label5;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private TextBox textBox6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox5;
+        private GroupBox groupBox1;
     }
 }
--- I.RULE CM Level Creator/Form1.cs
+++ I.RULE CM Level Creator/Form1.cs
@@ -6,11 +6,61 @@ namespace I.RULE_CM_Level_Creator
         String levelDescription = "";
         String levelNameColor = "";
         String levelDescriptionColor = "";
-        float levelHeartFallSpeed = 1;
+        double levelHeartFallSpeed = 1;
         int levelHeartValue = 50;
-        float levelHeartSize = 1;
+        double levelHeartSize = 1;
         int levelHeartTimer = 0;
         int levelStartingHearts = 50;
+        int levelHeartsCap = 9999;
+        int levelWaveAmount = 10;
+        int[] levelBigWaves = new int[5];
+        bool levelDrawWaveBorder = true;
+        bool levelDrawFlags = true;
+        bool levelDrawWaveBar = true;
+        double levelTimerSpeed = 0.5;
+        int levelSpawnDelay = -115;
+        String levelTimerSpeedAltCondition = "less";
+        int levelEnemiesForAltSpeed = 1;
+        int levelTimerSpeedAlt = 1;
+        int levelDefaultTimerAfterWave = -115;
+        int levelIntensity = 0;
+        double levelIntensityChangeMultiplyer = 1;
+        String levelBackground = "lvl1";
+        String levelMusic1 = "mus_ch1";
+        String levelMusic2 = "mus_ch1_layer";
+        int levelDrawTimer = 1; //true|false
+        int levelCampaignLocked = -1; // -1|1
+        int levelHiddenUntilUnlocked = 0;
+        int levelSpikeBabyDoesntBreakPoop = 0;
+        int levelDisableCHADScaling = 0; // 0|1 
+        int levelBobsBrainExtraHealth = 0;
+        int levelBobsBrainExplodeUponDeath = 0; // 0|1  checkbox: Killed only by self-descrution? Y/N 
+        int levelBobsBrainNoSound = 1; // 0|1  checkbox: Play Bob's Brain growing sound? Y/N
+        int levelFrozenSpriteCap = 2;
+        int defaultIcePoopDontConvertPoop = 0; // 0|1
+        int defaultIcePoopDontFreezeEnemies = 0; // 0|1
+        int defaultIcePoopFreezePower = 60;
+        double defaultIcePoopFreezePowerMult = 1;
+        int levelFlyDontBuzzEarly = 0;
+        double levelMaxBabies = 4;
+        int levelDisableCoinHUD = 1; //checkbox: Enable coin HUD? Y/N
+        int levelMulliboomDefaultExplosionBabyDamage = 2000;
+        int levelMulliboomDefaultExplosionTileDamage = 2000;
+        //other conditions to end level
+        int levelConditionEndWithHearts; //chechbox: Have at least X hearts at end of the level
+        int levelConditionCollectTotalHearts; //checkbox: Collect X hearts during level
+        int levelConditionMaxTotalHearts;
+        int levelConditionBabiesKilled; //checkbox: Don't let X or more babies  to be killed (Self-destructing, instant-use and shovelled babies count as dead as well)
+        int levelConditionMaxBabiesOnScreen;
+        String[] customSoundFiles = { "", "", "" };
+
+
+
+
+        //accepted values
+        String[] levelTimerSpeedAltConditionAV = { "less", "more" };
+        String[] levelBackgroundAV = { "lvl1", "lvl2", "lvl3", "megaPlum", "ragdoll", "middleHorn" };
+
         public Form1()
         {
             InitializeComponent();
@@ -28,12 +78,31 @@ namespace I.RULE_CM_Level_Creator
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text != "") {
-                levelName = textBox1.Text;
+            if (levelNameTextBox.Text != "")
+            {
+                levelName = levelNameTextBox.Text;
             }
-            MessageBox.Show("Level Name: " + levelName);
+            if (levelDescriptionTextBox.Text != "")
+            {
+                levelDescription = levelDescriptionTextBox.Text;
+            }
+            levelNameColor = colorDialog1.Color.ToString();
+            levelDescriptionColor = colorDialog2.Color.ToString();
             MessageBox.Show("Level saved!");
             Environment.Exit(0);
+        }
+
+        private Boolean isInArray(String[] array, String testedValue)
+        {
+            Boolean isAnElement = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (testedValue == array[i])
+                {
+                    isAnElement = true;
+                }
+            }
+            return isAnElement;
         }
     }
 }
